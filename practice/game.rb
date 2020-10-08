9be39fcf164c7aebd785f9c34c91746b531279ac
Rich T <richard.todd@shopify.com>
2020-10-08 19:01:51 -0400

    Generate random number

--- practice/game.rb
+++ practice/game.rb
@@ -7,3 +7,9 @@ print "What's your name? "
 name = gets.chomp
 
 puts "Welcome, #{name}!"
+
+# Store random number for the player to guess.
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
+
