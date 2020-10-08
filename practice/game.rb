712ab1f799fcbbff8c5f468e83dde03d0a0989cf
Rich T <richard.todd@shopify.com>
2020-10-08 19:08:45 -0400

    Finished game

--- practice/game.rb
+++ practice/game.rb
@@ -20,14 +20,22 @@ guessed_it = false
 oops_string = "Oops. You guess was"
 
 puts "#{10 - num_guesses} guesses left."
-print "Make a guess: "
-guess = gets.to_i
 
-if guess == target
-  puts "Good job, #{name}! You guessed my number in #{num_guesses} guesses!"
-  guessed_it = true;
-elsif guess < target
-  puts "#{oops_string} LOW"
-else
-  puts "#{oops_string} HIGH"
+until num_guesses == 10 || guessed_it
+  print "Make a guess: "
+  guess = gets.to_i
+  if guess == target
+    puts "Good job, #{name}! You guessed my number in #{num_guesses} guesses!"
+    guessed_it = true;
+  elsif guess < target
+    puts "#{oops_string} LOW"
+  else
+    puts "#{oops_string} HIGH"
+  end
+  num_guesses += 1
+end
+
+
+unless guessed_it
+  puts "Sorry you ran out of guesses. It was #{target}"
 end
