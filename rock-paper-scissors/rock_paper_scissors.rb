ce3f008980bee16fc13d44a95d9ae0d794e9e857
Rich T <richard.todd@shopify.com>
2020-10-19 17:25:04 -0400

    Replay

--- rock-paper-scissors/rock_paper_scissors.rb
+++ rock-paper-scissors/rock_paper_scissors.rb
@@ -9,38 +9,34 @@ def play(choice_1, choice_2)
   player1_score = 0
   player2_score = 0
   
-  if choice_1 == choice_2
-    puts "Tie!"
-  else
-    case choice_1
-    when "Rock"
-      if choice_2 == "Scissors"
-        puts rock_wins
-        player1_score += 1
-      else
-        puts paper_wins
-        player2_score += 1
-      end
-    when "Paper"
-      if choice_2 == "Rock"
-        puts paper_wins
-        player1_score += 1
-      else
-        puts scissors_win
-        player2_score += 1
-        
-      end
-    when "Scissors"
-      if choice_2 == "Paper"
-        puts scissors_win
-        player1_score += 1
-      else
-        puts rock_wins
-        player2_score += 1
-      end
+  case choice_1
+  when "Rock"
+    if choice_2 == "Scissors"
+      puts rock_wins
+      player1_score += 1
+    else
+      puts paper_wins
+      player2_score += 1
+    end
+  when "Paper"
+    if choice_2 == "Rock"
+      puts paper_wins
+      player1_score += 1
+    else
+      puts scissors_win
+      player2_score += 1
+      
+    end
+  when "Scissors"
+    if choice_2 == "Paper"
+      puts scissors_win
+      player1_score += 1
+    else
+      puts rock_wins
+      player2_score += 1
     end
   end
-  
+
   if player1_score > player2_score
     puts "Player 1 wins."
   elsif player2_score > player1_score
@@ -48,18 +44,29 @@ def play(choice_1, choice_2)
   end
 end
 
+def get_user_input
+  print "Player 1 - Enter your selection: "
+  first_input = gets.chomp.capitalize
+  
+  print "Player 2 - Enter your selection: "
+  second_input = gets.chomp.capitalize
+  
+  [first_input, second_input]
+end 
 
 puts "Rock Paper Scissors"
-print "Player 1 - Enter your selection: "
-player1_input = gets.chomp.capitalize
+player1_input, player2_input = get_user_input
 
-print "Player 2 - Enter your selection: "
-player2_input = gets.chomp.capitalize
 
 # End if choices are not valid
 unless validate_input(player1_input) && validate_input(player2_input)
   puts "No cheaters! Only Rock, Paper or Scissors are allowed."
-  return
+  player1_input, player2_input = get_user_input
+end
+
+if player1_input == player2_input
+  puts "Tie!"
+  player1_input, player2_input = get_user_input
 end
 
 
