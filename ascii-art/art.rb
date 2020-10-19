701cdf582ea6c1541939322756092a7697d5284e
Rich T <richard.todd@shopify.com>
2020-10-19 16:18:28 -0400

    Added triangle method

--- ascii-art/art.rb
+++ ascii-art/art.rb
@@ -1,23 +1,25 @@
+def create_triangle(sides)
+  for num in 1..sides do
+    print " " * (sides - num)
+    puts "*" * num
+   end
+end
+
+
 def ascii_art
   print "Please enter a number to create a triangle: "
 
   input = gets.chomp
+  input_int = input.to_i
   
    # validate input
    # check if input is string or float or 0 of below
-   while input.to_i <= 0 || input.to_i.to_s != input 
+   while input_int <= 0 || input_int.to_s != input 
      print "Not an integer, please enter an integer: "
      input = gets.chomp
    end 
    
-   input_int = input.to_i
-   
-   # generate triangle
-   for num in 1..input_int do
-    print " " * (input_int - num)
-    puts "*" * num
-   end
-  
+   create_triangle(input_int)
 end
 
 ascii_art
