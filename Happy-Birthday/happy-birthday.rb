98c8cbfa8f27e8ccef7da5f7e545da1992b39f85
Rich T <richard.todd@shopify.com>
2020-10-19 14:32:31 -0400

    Happy birthday while/if

--- Happy-Birthday/happy-birthday.rb
+++ Happy-Birthday/happy-birthday.rb
@@ -1,5 +1,15 @@
 print "What is your name? "
-
 name = gets.chomp
 
-puts "Happy birthday to you\n Happy birthday to you\n Happy birthday dear #{name}\n Happy birthday to you."
+happy_birthday_string = "Happy birthday "
+
+i = 0
+while i < 4
+  print happy_birthday_string
+  if i == 2
+    puts "dear #{name}"
+  else
+    puts "to you"
+  end
+  i+=1
+end
