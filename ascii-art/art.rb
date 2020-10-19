431741edc698573f01cbf60ec26b0d72a8f32573
Rich T <richard.todd@shopify.com>
2020-10-19 16:06:35 -0400

    ASCII art part 1

---
+++ ascii-art/art.rb
@@ -0,0 +1,18 @@
+def ascii_art
+  print "Please enter a number to create a triangle: "
+
+  input = gets.chomp
+  
+   # validate input
+  
+   
+   input_int = input.to_i
+   
+   # generate triangle
+   for num in 1..input_int do
+    puts "*" * num
+   end
+  
+end
+
+ascii_art
