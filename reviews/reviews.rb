842b3118179564fc821684637a5f2f85fef282e5
Rich T <richard.todd@shopify.com>
2020-10-22 18:06:31 -0400

    find_adjective method

--- reviews/reviews.rb
+++ reviews/reviews.rb
@@ -3,7 +3,18 @@ File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
 relevant_lines = lines.find_all{ |line| line.include?("Truncated")}
 reviews = relevant_lines.reject{ |line| line.include?("--")}
+adjectives = []
 
-puts reviews
+reviews.each do |review|
+  adjectives << find_adjective(review)
+end
+
+puts adjectives
