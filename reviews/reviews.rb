65f7ac432194cc964a90b12421faf8462820aecc
Rich T <richard.todd@shopify.com>
2020-10-22 18:00:48 -0400

    Removed bylines

--- reviews/reviews.rb
+++ reviews/reviews.rb
@@ -4,5 +4,6 @@ File.open("reviews.txt") do |review_file|
 end
 
 relevant_lines = lines.find_all{ |line| line.include?("Truncated")}
+reviews = relevant_lines.reject{ |line| line.include?("--")}
 
-puts relevant_lines
+puts reviews
