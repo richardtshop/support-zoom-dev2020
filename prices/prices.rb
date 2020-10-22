aafb7cba85171e80019310c096c88dfbc84e4ff9
Rich T <richard.todd@shopify.com>
2020-10-22 16:04:10 -0400

    DRY total

--- prices/prices.rb
+++ prices/prices.rb
@@ -1,19 +1,15 @@
 def total(prices)
   amount = 0
-  index = 0
-  while index < prices.length
-    amount += prices[index]
-    index += 1;
+  prices.each do |price|
+    amount += price
   end
   amount
 end
 
 def total(prices)
   amount = 0
-  index = 0
-  while index < prices.length
-    amount -= prices[index]
-    index += 1;
+  prices.each do |price|
+    amount -= price
   end
   amount
 end
