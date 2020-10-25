5bd39123105ab54b8563ffb83394e02aa270799f
Rich T <richard.todd@shopify.com>
2020-10-25 18:40:42 -0400

    Added truck methods

--- vehicles/vehicles.rb
+++ vehicles/vehicles.rb
@@ -17,3 +17,35 @@ class Vehicle
     puts "Turn front 2 wheels"
   end
 end
+
+class Car < Vehicle
+end
+
+class Truck < Vehicle
+  attr_accessor :cargo
+  
+  def load_bed(contents)
+    puts "Securing #{contents} in the truck bed."
+    @cargo = contents
+  end
+end
+
+class Motorcycle < Vehicle
+end
+
+car = Car.new
+car.odometer = 11432
+car.gas_used = 366
+
+puts "Lifetime MPG:"
+puts car.mileage
+
+truck = Truck.new
+truck.load_bed("259 bouncy balls")
+puts "The truck is carrying #{truck.cargo}."
+
+
+truck.odometer = 11432
+truck.gas_used = 366
+puts "Lifetime MPG:"
+puts truck.mileage
