6dc0aeffc7ce8adb567b2e6dfc4d00c9ed9029e8
Rich T <richard.todd@shopify.com>
2020-10-26 12:51:03 -0400

    Two methods

--- 3/Cashmoney/change2.rb
+++ 3/Cashmoney/change2.rb
@@ -1,11 +1,20 @@
-# This version creates empty hash to store all possible coins and filters result
+# This version creates a hash to store coin values during loop and outputs result
 
-def calculate_coins(total_cents, coin_values, change_coins)
+def calculate_coins(total_cents, coin_values)
+  change_coins = {}
   until total_cents < 5
     current_coin = coin_values.find { |_key, value| value <= total_cents }
-    change_coins[current_coin.first] += 1
-    total_cents -= current_coin[1]
+    current_coin_key, current_coin_value = current_coin
+
+    if change_coins[current_coin_key].nil?
+      change_coins[current_coin_key] = 1
+    else
+      change_coins[current_coin_key] += 1
+    end
+    
+    total_cents -= current_coin_value
   end
+
   change_coins[:nickel] += 1 if [3, 4].include?(total_cents) # Nickel rounding
   change_coins
 end
@@ -27,24 +36,15 @@ coin_values = {
   nickel: 5
 }
 
-change_coins = {
-  toonie: 0,
-  loonie: 0,
-  quarter: 0,
-  dime: 0,
-  nickel: 0
-}
-
 total_cents = (total_dollars * 100).to_i
 
-change = calculate_coins(total_cents, coin_values, change_coins)
+change = calculate_coins(total_cents, coin_values)
+
 coin_count = change.values.reduce(:+)
 print 'You need to dispense '
 
-change.each_with_index do |(key, value), index|
+change.each_with_index do |(key, value), index| 
   last = index == change.size - 1
-  next unless value.positive?
-
   print 'and ' if last
   print "#{value} #{key}#{value > 1 ? 's' : ''}"
   print last ? '.' : ', '
