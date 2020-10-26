4fca6d29e2459434643c575c09ee99d8710a982f
Rich T <richard.todd@shopify.com>
2020-10-25 21:56:30 -0400

    Added user and UI

--- little-free-library/library.rb
+++ little-free-library/library.rb
@@ -1,12 +1,44 @@
-#### TO DO
-# Build out user class
-# Build out UI
-# use nil? method to check for nil
-
-# User has name and array of books attributes
-# Look - returns all books in a library array
-
 class User
+  attr_reader :name, :books
+  
+  def initialize(name)
+    @name = name
+    @books = [Book.new("Dave",200)]
+  end
+
+  def take_book(library, book_title)
+    removed_book = library.remove_book(book_title)
+    
+    if removed_book.nil?
+      puts"That book isn't in the library."
+    else
+      @books << removed_book
+      puts "#{removed_book.title} has been added to your current books."
+    end
+  end
+  
+  def place_book(library, book_title)
+    if (book_title.is_a? String) && (book_title.length > 0)
+      book = @books.find{|book| book.title.downcase == book_title.downcase}
+    end
+    
+    if book == nil
+      puts "You don't currently have that book."
+    else
+      book_added = library.add_book(book)
+      if book_added
+        @books.delete(book)
+        puts "You no longer have #{book.title}."
+      end
+    end
+  end
+  
+  def view_current_books
+    return puts "You currently have no books." if @books.size == 0
+    puts "These are your current books:"
+    @books.each_with_index {|book, index| puts "#{index+1}: #{book.title} (#{book.pages} pages)"}
+    puts "\n"
+  end
 end
 
 class Book
@@ -46,11 +78,12 @@ class Library
     else
       puts "There are currently #{@books.length} books in the library:"
       @books.each_with_index {|book, index| puts "#{index+1}: #{book.title} (#{book.pages} pages)"}
+      puts "\n"
     end
   end
   
   def add_book(book)
-    if @books.length > MAX_BOOKS  
+    if @books.length >= MAX_BOOKS  
       puts "The library is full."
       return false 
     end
@@ -61,14 +94,13 @@ class Library
   end
   
   def remove_book(book_title)
-    if book_title.is_a? String && book_title.length > 0
-      book_index = @books.find_index{|book| book.title.downcase == book_title.downcase}
+    if (book_title.is_a? String) && (book_title.length > 0)
+      book = @books.find{|book| book.title.downcase == book_title.downcase}
     end
-    
-    unless book_index == nil
-      @books.delete_at(book_index)
+    unless book == nil
+      puts "#{book.title} removed from library."
+      @books.delete(book)
     end
-    # @books.delete_at(book_index) unless book_index == nil
   end
 end
 
@@ -88,15 +120,58 @@ books = [
   Book.new("The Prince", 201),
   Book.new("How I Manage",3),
   Book.new("Charlotte's Webb", 150),
-  Book.new("The Northern Lights", 420),
+  Book.new("The Northern Lights", 424),
+  Book.new("The Subtle Knife", 502),
   Book.new("La Belle Savage", 395)
 ]
 
 library = Library.new(books)
 
-library.look
-# library.add_book(Book.new("Chicken and the Egg", 100))
-puts library.remove_book("The Way of Kings").title # Returns book title
-puts library.remove_book(1) # Returns nothing
-library.look
+def menu_choices(choice, library, user)
+  case choice
+  when 1
+    library.look
+  when 2
+    print "Enter the title of the book you would like to take: "
+    title = gets.chomp
+    user.take_book(library, title)
+  when 3
+    return puts "You currently have no books." if user.books.size == 0
+    user.view_current_books
+    print "Enter the title of the book you would like to place in the library: "
+    title = gets.chomp
+    user.place_book(library, title)
+  end
+end
 
+puts "Welcome to the little library"
+print "What is your name: "
+
+name = gets.chomp
+
+user = User.new(name == "" ? "anonymous" : name)
+puts "Hello, #{user.name}!"
+
+loop do
+
+  puts "What would you like to do?"
+  puts "1. View all books in the library"
+  puts "2. Take a book from the library"
+  puts "3. Place a book in the library"
+  puts "4. Exit"
+  print "Please enter your choice: "
+
+  choice = gets.chomp.to_i
+
+  while choice <= 0 || choice > 4
+    puts "Invalid choice, please try again"
+    choice = gets.chomp.to_i
+  end
+
+  if choice == 4
+    puts "Goodbye"
+    break
+  end
+  
+  menu_choices(choice, library, user)
+end
