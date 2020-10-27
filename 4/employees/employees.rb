fef7c11f755abdb9d6a01dc476a54f394bdf821a
Rich T <richard.todd@shopify.com>
2020-10-26 21:41:31 -0400

    Added HourlyEmployee class

--- 4/employees/employees.rb
+++ 4/employees/employees.rb
@@ -25,7 +25,32 @@ class SalariedEmployee < Employee
   end
 end
 
+class HourlyEmployee
+  attr_reader :hourly_wage, :hours_week
+
+  def hourly_wage=(hourly_wage)
+    # Code to validated and set @hourly_wage
+  end
+
+  def hours_per_week
+    # Code to validated and set @hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+
 salaried_employee = SalariedEmployee.new
 salaried_employee.name = "Jane Doe"
 salaried_employee.salary = 50000
 salaried_employee.print_pay_stub
+
+hourly_employee = HourlyEmployee.new
+hourly_employee.name = "Jane Smith"
+hourly_employee.hourly_wage = 14.97
+hourly_employee.hours_per_week = 30
+hourly_employee.print_pay_stub
