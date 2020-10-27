088c6009409b48d04c58b1266976f48c7f3960fd
Rich T <richard.todd@shopify.com>
2020-10-26 22:35:48 -0400

    Added class factory methods

--- 4/employees/employees.rb
+++ 4/employees/employees.rb
@@ -1,33 +1,30 @@
 class Employee
   attr_reader :name
 
-  def name=(name)
-    # Code to validate and set @name
-    @name = name
-  end
+  attr_writer :name
 
   def initialize(name)
+    raise "Name can't be blank" if name == ""
     self.name = name
   end
-  
+
   def print_name
     puts "Name: #{name}"
   end
-  
+
   def print_pay_stub(pay_for_period)
     print_name
     formatted_pay = format("$%.2f", pay_for_period)
     puts "Pay This Period: #{formatted_pay}"
   end
-  
 end
 
 class SalariedEmployee < Employee
   attr_reader :salary
 
   def salary=(salary)
-    # Code to validate and set @salary
-    @salary = salary
+    raise "A salary of #{salary} isn't valid!" if salary.negative?
+    self.salary = salary
   end
 
   def initialize(name = "Anonymous", salary = 0.0)
@@ -41,16 +38,29 @@ class SalariedEmployee < Employee
 end
 
 class HourlyEmployee < Employee
+  # Factory methods
+  def self.security_guard(name)
+    HourlyEmployee.new(name, 19.25, 30)
+  end
+
+  def self.cashier(name)
+    HourlyEmployee.new(name, 12.75, 30)
+  end
+
+  def self.janitor(name)
+    HourlyEmployee.new(name, 10.05, 30)
+  end
+
   attr_reader :hourly_wage, :hours_per_week
 
   def hourly_wage=(hourly_wage)
-    # Code to validated and set @hourly_wage
-    @hourly_wage = hourly_wage
+    raise "An hourly wage of #{hourly_wage} isn't valid!" if hourly_wage.negative?
+    self.salary = salary
   end
 
   def hours_per_week=(hours_per_week)
-    # Code to validated and set @hours_per_week
-    @hours_per_week = hours_per_week
+    raise "Hours a week of #{hours_per_week} isn't valid!" if hours_per_week.negative?
+    self.hours_per_week = hours_per_week
   end
 
   def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0)
@@ -70,14 +80,10 @@ salaried_employee.print_pay_stub
 hourly_employee = HourlyEmployee.new("Jane Smith", 14.97, 30)
 hourly_employee.print_pay_stub
 
+ivan = HourlyEmployee.cashier("Ivan Stokes")
+edwin = HourlyEmployee.janitor("Edwin Bugress")
+angela = HourlyEmployee.security_guard("Angela Matthews")
 
-ivan = HourlyEmployee.new("Ivan Stokes", 12.75, 25) 
-harold = HourlyEmployee.new("Harold Nguyen", 12.75, 25)
-tamara = HourlyEmployee.new("Tamara Wells", 12.75, 25)
-susie = HourlyEmployee.new("Susie Powell", 12.75, 25)
-
-edwin = HourlyEmployee.new("Edwin Bugress", 10.50, 20)
-ethel = HourlyEmployee.new("Ethel Harris", 10.50, 20)
-
-angela = HourlyEmployee.new("Angela Matthews", 19.25, 30)
-stewart = HourlyEmployee.new("Stewart Sanchez", 19.25, 30)
+angela.print_pay_stub
+edwin.print_pay_stub
+ivan.print_pay_stub
