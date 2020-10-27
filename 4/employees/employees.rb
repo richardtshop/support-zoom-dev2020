606a3b85879ebe9d9d480f3ce863b7fce52a530e
Rich T <richard.todd@shopify.com>
2020-10-26 21:57:03 -0400

    Added initialize methods back in

--- 4/employees/employees.rb
+++ 4/employees/employees.rb
@@ -3,6 +3,7 @@ class Employee
 
   def name=(name)
     # Code to validate and set @name
+    @name = name
   end
 
   def print_name
@@ -15,25 +16,39 @@ class SalariedEmployee < Employee
 
   def salary=(salary)
     # Code to validate and set @salary
+    @salary = salary
+  end
+
+  def initialize(name = "Anonymous", salary = 0.0)
+    self.name = name
+    self.salary = salary
   end
 
   def print_pay_stub
     print_name
-    pay_for_period = (salaray / 365.0) * 14
+    pay_for_period = (salary / 365.0) * 14
     formatted_pay = format("$%.2f", pay_for_period)
     puts "Pay This Period: #{formatted_pay}"
   end
 end
 
-class HourlyEmployee
-  attr_reader :hourly_wage, :hours_week
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
 
   def hourly_wage=(hourly_wage)
     # Code to validated and set @hourly_wage
+    @hourly_wage = hourly_wage
   end
 
-  def hours_per_week
+  def hours_per_week=(hours_per_week)
     # Code to validated and set @hours_per_week
+    @hours_per_week = hours_per_week
+  end
+
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0)
+    self.name = name
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
   end
 
   def print_pay_stub
@@ -44,13 +59,8 @@ class HourlyEmployee
   end
 end
 
-salaried_employee = SalariedEmployee.new
-salaried_employee.name = "Jane Doe"
-salaried_employee.salary = 50000
+salaried_employee = SalariedEmployee.new("Jane Doe", 50000)
 salaried_employee.print_pay_stub
 
-hourly_employee = HourlyEmployee.new
-hourly_employee.name = "Jane Smith"
-hourly_employee.hourly_wage = 14.97
-hourly_employee.hours_per_week = 30
+hourly_employee = HourlyEmployee.new("Jane Smith", 14.97, 30)
 hourly_employee.print_pay_stub
