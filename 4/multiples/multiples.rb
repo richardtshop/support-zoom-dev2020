2258149915c7b532376cb77a610d12d6d8541ec7
Rich T <richard.todd@shopify.com>
2020-10-26 23:32:18 -0400

    Multiples with blocks

--- 4/multiples/multiples.rb
+++ 4/multiples/multiples.rb
@@ -1,7 +1,18 @@
 def customized_list_of_multiples(multiples_of, size)
   list = Array.new(size) { |index| (index + 2) * multiples_of }
-  yield(list) if block_given?
-  # need to add this as yield evaluated as nil if no block given
-  # and nil returned
-  list
+  if block_given?
+    yield list
+  else
+    # need to add this as yield evaluated as nil if no block given
+    # and nil returned
+    list
+  end
 end
+
+p customized_list_of_multiples(3, 6)
+
+p customized_list_of_multiples(3, 6) { |arr| arr.map(&:to_s) }
+
+doubler = proc { |el| el * 2 }
+p customized_list_of_multiples(3, 6) { |arr| arr.map(&doubler) }
+
