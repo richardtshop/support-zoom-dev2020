1e05f1d4bdabfb2eb52800d7dfb86cd45d8c00c8
Rich T <richard.todd@shopify.com>
2020-10-27 14:04:01 -0400

    print error

--- 4/multiples/multiples.rb
+++ 4/multiples/multiples.rb
@@ -16,3 +16,9 @@ p customized_list_of_multiples(3, 6) { |arr| arr.map(&:to_s) }
 doubler = proc { |el| el * 2 }
 p customized_list_of_multiples(3, 6) { |arr| arr.map(&doubler) }
 
+# block_given? returns false for this
+# Because print method call only passes the first line of code to prin
+# Need to add parenthesis ()
+print customized_list_of_multiples(3, 6) do |arr|
+  arr.map(&doubler)
+end
