967f9bc2bc3a4ddff534e840da567affa8dd7314
Rich T <richard.todd@shopify.com>
2020-10-28 16:31:25 -0400

    Custom each method

--- Week_5/5-Code_along-Comparable_and_Enumerable/compenum.rb
+++ Week_5/5-Code_along-Comparable_and_Enumerable/compenum.rb
@@ -1,3 +1,5 @@
+# Comparable module
+
 class Steak
   
   include Comparable
@@ -14,6 +16,8 @@ class Steak
     else
       return 1
     end
+    # Alternate method that does the same as the above
+    # GRADE_SCORES[grade] <=> GRADE_SCORES[other.grade]
   end
 end
 
@@ -24,10 +28,31 @@ choice.grade = "Choice"
 select = Steak.new
 select.grade = "Select"
 
-puts "prime > choice: #{prime > choice}"
-puts "prime < select: #{prime < select}"
-puts "select == select: #{select == select}"
-puts "select <= select: #{select <= select}"
-puts "select >= choice: #{select >= choice}"
-print "choice.between?(select, prime):"
-puts choice.between?(select, prime)
+# puts "prime > choice: #{prime > choice}"
+# puts "prime < select: #{prime < select}"
+# puts "select == select: #{select == select}"
+# puts "select <= select: #{select <= select}"
+# puts "select >= choice: #{select >= choice}"
+# print "choice.between?(select, prime):"
+# puts choice.between?(select, prime)
+
+# Enumerable module code
+
+class WordSplitter
+  include Enumerable # need to include to be able to user Enumerable methods on objects of this class
+
+  attr_accessor :string
+
+  def each
+    string.split(' ').each do  |word| 
+      yield word
+    end
+  end
+end
+
+splitter = WordSplitter.new
+splitter.string = "one two three four"
+
+splitter.each do |word|
+  puts word
+end
