1bbaa079ebe8ac371e3f465f478823ab711e07f8
Rich T <richard.todd@shopify.com>
2020-10-28 16:20:17 -0400

    Added comparable tests

--- Week_5/5-Code_along-Comparable_and_Enumerable/compenum.rb
+++ Week_5/5-Code_along-Comparable_and_Enumerable/compenum.rb
@@ -1,11 +1,13 @@
-# Don't need to include the enumerable mixin as the Numeric class has it already, and that's what we are comparing
-
 class Steak
+  
+  include Comparable
+  
   GRADE_SCORES = { "Prime" => 3, "Choice" => 2, "Select" => 1 }
+  
   attr_accessor :grade
   
   def <=>(other)
-    if GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+    if GRADE_SCORES[grade] < GRADE_SCORES[other.grade]
       return -1
     elsif GRADE_SCORES[grade] == GRADE_SCORES[other.grade]
       return 0
@@ -15,11 +17,17 @@ class Steak
   end
 end
 
-first_steak = Steak.new
-first_steak.grade = "Prime"
+prime = Steak.new
+prime.grade = "Prime"
+choice = Steak.new
+choice.grade = "Choice"
+select = Steak.new
+select.grade = "Select"
 
-second_steak = Steak.new
-second_steak.grade = "Choice"
-
-puts first_steak <=> second_steak
-puts second_steak <=> first_steak  
+puts "prime > choice: #{prime > choice}"
+puts "prime < select: #{prime < select}"
+puts "select == select: #{select == select}"
+puts "select <= select: #{select <= select}"
+puts "select >= choice: #{select >= choice}"
+print "choice.between?(select, prime):"
+puts choice.between?(select, prime)
