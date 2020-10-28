82a9f24e25c8b2e33236f816a77633111e6dfe4f
Rich T <richard.todd@shopify.com>
2020-10-28 12:19:34 -0400

    Moved methods to module

--- Week_5/2-Code_along-Media/media.rb
+++ Week_5/2-Code_along-Media/media.rb
@@ -8,7 +8,7 @@ module AcceptsComments
   end 
   
   def add_comment(comment)
-    @comments << comment
+    comments << comment # don't need to use self.comments as not using assignment operator
   end
 end
 
@@ -19,11 +19,20 @@ class Clip
 end
 
 class Video < Clip
+  include AcceptsComments
   attr_accessor :resolution
 end
 
 class Song < Clip
+  include AcceptsComments
   attr_accessor :beats_per_minute
+end
+
+class Photo
+  include AcceptsComments
+  def show
+    puts "Displaying #{object_id}..."
+  end
 end
 
 video = Video.new
@@ -31,5 +40,13 @@ video.add_comment("Cool slow motion effect!")
 video.add_comment("Weird ending")
 song = Song.new
 song.add_comment("Awesome beat.")
+photo = Photo.new
+photo.add_comment("Beautiful comments")
 
 p video.comments, song.comments
+
+video.play
+song.play
+photo.show
+
+p photo.comments
