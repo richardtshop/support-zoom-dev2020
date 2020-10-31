97cfe37b866e885dff68389efa7fff6da9cc2351
Rich T <richard.todd@shopify.com>
2020-10-31 12:51:24 -0400

    To string method

--- Week_5/8-Dominos/dominos.rb
+++ Week_5/8-Dominos/dominos.rb
@@ -1,25 +1,41 @@
 class Domino
   attr_reader :top, :bottom
- 
-  def initialize(top, bottom)
-    validate(input)
-    validate(bottom)
-    @top = top
-    @bottom = bottom
 
+  DOMINO_PATTERNS = [
+    ["|     |", "|     |", "|     |"],
+    ["|     |", "|  *  |", "|     |"],
+    ["|*    |", "|     |", "|    *|"],
+    ["|*    |", "|  *  |", "|    *|"],
+    ["|*   *|", "|     |", "|*   *|"],
+    ["|*   *|", "|  *  |", "|*   *|"],
+    ["|*   *|", "|*   *|", "|*   *|"],
+  ]
+
+  def initialize(top, bottom)
+    @top = top if validate_domino_half(top)
+    @bottom = bottom if validate_domino_half(bottom)
+  end
+
+  def to_s
+    puts " ----- "
+    DOMINO_PATTERNS[top].each { |row| puts row }
+    puts " ----- "
+    DOMINO_PATTERNS[bottom].each { |row| puts row }
+    puts " ----- "
   end
 
   private
 
-  def validate_half(number)
+  def validate_domino_half(number)
     valid = number.is_a?(Integer) && number.between?(1, 6)
-    raise "" unless valid
+    puts "Invalid input: #{number}. Number must be between 1 and 6." unless valid
     valid
-    # number has to be
-    # an integer
-    # between 1 and 6
   end
 end
 
-puts 1.between?(1, 6)
-puts 6.between?(1, 6)
+domino = Domino.new(3, 6)
+
+p domino.top
+p domino.bottom
+
+domino.to_s
