8528c50b7eaeb49c3297ac4503f1089d2037ae95
Rich T <richard.todd@shopify.com>
2020-11-05 17:40:54 -0500

    Left triangle failing test

--- Week_6/6-Code_Along-Test_driven_development/tdd-ascii/ascii_art.rb
+++ Week_6/6-Code_Along-Test_driven_development/tdd-ascii/ascii_art.rb
@@ -1,17 +1,7 @@
 class AsciiArt
-  def draw(number)
+  def self.draw(number)
   end
 end
 
-print("Please enter a number to create a triangle: ")
-input = gets.chomp
-input_int = input.to_i
-
-while input_int < 0 || input_int.to_s != input
-  print("Not an integer, please enter an integer: ")
-  input = gets.chomp
-  input_int = input.to_i
-end
-
-diamond = AsciiArt.new
-puts diamond.draw(input_int)
+# diamond = AsciiArt.new
+# puts diamond.draw(10)
--- Week_6/6-Code_Along-Test_driven_development/tdd-ascii/tests/ascii_tests.rb
+++ Week_6/6-Code_Along-Test_driven_development/tdd-ascii/tests/ascii_tests.rb
@@ -1,2 +1,18 @@
 require 'minitest/autorun'
-require_relative '../acsii-art'
+require_relative '../ascii_art.rb'
+
+class AsciiTest < Minitest::Test
+  def setup
+    @art_0 = AsciiArt.draw(0)
+    @art_1 = AsciiArt.draw(1)
+    @art_2 = AsciiArt.draw(2)
+    @art_3 = AsciiArt.draw(3)
+    @art_4 = AsciiArt.draw(4)
+  end
+
+  def test_triangle
+    expected = ""
+    actual = @art_0
+    assert_equal(expected, actual)
+  end
+end
