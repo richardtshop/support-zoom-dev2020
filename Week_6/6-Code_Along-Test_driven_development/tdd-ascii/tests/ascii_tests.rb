bea8003ab226f5e23b2515a1d2fa94e11f31eae8
Rich T <richard.todd@shopify.com>
2020-11-05 22:28:46 -0500

    Added empty diamond test

--- Week_6/6-Code_Along-Test_driven_development/tdd-ascii/tests/ascii_tests.rb
+++ Week_6/6-Code_Along-Test_driven_development/tdd-ascii/tests/ascii_tests.rb
@@ -10,11 +10,16 @@ class AsciiTest < Minitest::Test
     @art_right_0 = AsciiArt.draw_right_triangle(0)
     @art_right_1 = AsciiArt.draw_right_triangle(1)
     @art_right_2 = AsciiArt.draw_right_triangle(2)
-    
+
     @art_centered_0 = AsciiArt.draw_centered_triangle(0)
     @art_centered_1 = AsciiArt.draw_centered_triangle(1)
     @art_centered_2 = AsciiArt.draw_centered_triangle(2)
     @art_centered_10 = AsciiArt.draw_centered_triangle(7)
+
+    @art_diamond_0 = AsciiArt.draw_diamond(0)
+    @art_diamond_1 = AsciiArt.draw_diamond(1)
+    @art_diamond_2 = AsciiArt.draw_diamond(2)
+    @art_diamond_10 = AsciiArt.draw_diamond(7)
   end
 
   def test_empty_left_triangle
@@ -40,7 +45,7 @@ class AsciiTest < Minitest::Test
     actual = @art_right_0
     assert_equal(expected, actual)
   end
- 
+
   def test_single_row_right_triangle
     expected = "*\n"
     actual = @art_right_1
@@ -52,23 +57,28 @@ class AsciiTest < Minitest::Test
     actual = @art_right_2
     assert_equal(expected, actual)
   end
-  
+
   def test_empty_centered_triangle
     expected = ""
     actual = @art_centered_0
     assert_equal(expected, actual)
   end
-  
+
   def test_single_centered_triangle
     expected = "*\n"
     actual = @art_centered_1
     assert_equal(expected, actual)
   end
-  
+
   def test_seven_row_centered_triangle
     expected = "      *\n     * *\n    * * *\n   * * * *\n  * * * * *\n * * * * * *\n* * * * * * *\n"
     actual = @art_centered_10
     assert_equal(expected, actual)
   end
-  
+
+  def test_empty_diamond
+    expected = ""
+    actual = @art_diamond_0
+    assert_equal(expected, actual)
+  end
 end
