c46ad60fea741a28cf023e020c50f2466f4ef923
Rich T <richard.todd@shopify.com>
2020-11-10 01:01:57 -0500

    Removed class tests

--- Week_8/4-Student_body/student-body/student_body.rb
+++ Week_8/4-Student_body/student-body/student_body.rb
@@ -11,14 +11,3 @@ class StudentBody
     @classes.each_pair { |_key, students| students.each { |student| yield student } }
   end
 end
-
-# school = StudentBody.new(
-#   classes: {
-#     class1: ["William", "Pavel", "Jean Luc", "Mr"],
-#     class2: ["James", "Leonard", "Montgomery", "Tasha"],
-#     class3: ["Nyota", "Hikaru", "Worf", "Geordie"],
-#     class4: ["Dianna", "Beverly", "Wesley", "Miles"],
-#   }
-# )
-
-# school.each { |student| puts student }
